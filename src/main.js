97cca5c62bfa9853a1deb652c8a5cde6fd8dd367
noman_hassan <nomanhassan092@gmail.com>
2017-06-30 20:04:34 +0500

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './twitterstream';
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
